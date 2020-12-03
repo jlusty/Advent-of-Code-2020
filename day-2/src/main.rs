9b1cf2154365622638c858cf0ce115b19032b5d1
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-12-03 08:57:25 +0000

    day 2 part 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn read_file(filename: String) -> Result<String, Box<dyn Error>> {
@@ -11,7 +12,7 @@ fn read_file(filename: String) -> Result<String, Box<dyn Error>> {
 }
 
 #[derive(Debug)]
-struct Policy {
+struct Policy1 {
     min: u32,
     max: u32,
     character: char,
@@ -25,7 +26,7 @@ fn part_1() {
     for line in input.lines() {
         let policy_vec: Vec<&str> = line.split(|c| c == '-' || c == ':' || c == ' ').collect();
         let char_vec: Vec<char> = policy_vec[2].chars().collect();
-        let policy = Policy {
+        let policy = Policy1 {
             min: policy_vec[0].parse().unwrap(),
             max: policy_vec[1].parse().unwrap(),
             character: char_vec[0],
@@ -42,6 +43,45 @@ fn part_1() {
 
         if valid {
             num_valid += 1;
+        }
+    }
+
+    println!("{}", num_valid);
+}
+
+#[derive(Debug)]
+struct Policy2 {
+    pos1: u32,
+    pos2: u32,
+    character: char,
+    pwd: String,
+}
+
+fn part_2() {
+    let input = read_file("./input.txt".to_string()).unwrap();
+
+    let mut num_valid = 0;
+    for line in input.lines() {
+        let policy_vec: Vec<&str> = line.split(|c| c == '-' || c == ':' || c == ' ').collect();
+        let char_vec: Vec<char> = policy_vec[2].chars().collect();
+        let policy = Policy2 {
+            pos1: policy_vec[0].parse().unwrap(),
+            pos2: policy_vec[1].parse().unwrap(),
+            character: char_vec[0],
+            pwd: policy_vec[4].to_owned(),
+        };
+        let char1option: Option<char> = policy.pwd.chars().nth(policy.pos1 as usize - 1);
+        let char2option: Option<char> = policy.pwd.chars().nth(policy.pos2 as usize - 1);
+        if char1option.is_some() && char2option.is_some() {
+            let char1 = char1option.unwrap();
+            let char2 = char2option.unwrap();
+
+            let valid = (char1 == policy.character && char2 != policy.character)
+                || (char1 != policy.character && char2 == policy.character);
+
+            if valid {
+                num_valid += 1;
+            }
         }
     }
 
