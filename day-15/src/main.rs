9aa369ff5ac249f595fc64808aa141004277be00
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-12-15 23:48:32 +0000

    Day 15 part 2 (+ part 1 improved)

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn read_file(filename: String) -> Result<String, Box<dyn Error>> {
@@ -22,25 +24,55 @@ fn part_1() {
     }
     // println!("{:?}", num_vec);
 
-    let mut nums_spoken = vec![0];
+    let mut nums_spoken = HashMap::<usize, usize>::new();
+    let mut last_spoken = 0;
+    let mut last_spoken_to_insert = last_spoken;
     for i in 0..2020 {
         if i < num_vec.len() {
-            nums_spoken.push(num_vec[i])
+            last_spoken = num_vec[i];
         } else {
-            let mut i = nums_spoken.len() - 2;
-            while i > 0 {
-                if nums_spoken[i] == nums_spoken[nums_spoken.len() - 1] {
-                    // println!("spoken before: {}", nums_spoken[i]);
-                    nums_spoken.push(nums_spoken.len() - 1 - i);
-                    break;
-                }
-                i -= 1;
-            }
-            if i == 0 {
-                nums_spoken.push(0);
+            last_spoken = *nums_spoken.get(&last_spoken).unwrap_or(&0);
+            if last_spoken != 0 {
+                last_spoken = i - last_spoken;
             }
         }
         // println!("{}", nums_spoken[nums_spoken.len() - 1]);
+        nums_spoken.insert(last_spoken_to_insert, i);
+        last_spoken_to_insert = last_spoken;
     }
-    println!("{}", nums_spoken[nums_spoken.len() - 1]);
+    println!("{}", last_spoken);
+}
+
+fn part_2() {
+    let input = read_file("./input.txt".to_string()).unwrap();
+
+    let mut num_vec = Vec::<usize>::new();
+    for line in input.lines() {
+        num_vec = line
+            .split(',')
+            .map(|n| n.parse().unwrap())
+            .collect::<Vec<usize>>();
+    }
+    // println!("{:?}", num_vec);
+
+    let mut nums_spoken = HashMap::<usize, usize>::new();
+    let mut last_spoken = 0;
+    let mut last_spoken_to_insert = last_spoken;
+    for i in 0..30000000 {
+        if i < num_vec.len() {
+            last_spoken = num_vec[i];
+        } else {
+            last_spoken = *nums_spoken.get(&last_spoken).unwrap_or(&0);
+            if last_spoken != 0 {
+                last_spoken = i - last_spoken;
+            }
+        }
+        // println!("{}", nums_spoken[nums_spoken.len() - 1]);
+        nums_spoken.insert(last_spoken_to_insert, i);
+        last_spoken_to_insert = last_spoken;
+        if i % 1000000 == 0 {
+            println!("{}", i)
+        }
+    }
+    println!("{}", last_spoken);
 }
