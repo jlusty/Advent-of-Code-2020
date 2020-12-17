a174ff4a906693681cc82fd8fbc614a9897d559b
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-12-17 20:13:54 +0000

    Day 17 part 1

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
@@ -14,95 +13,115 @@ fn read_file(filename: String) -> Result<String, Box<dyn Error>> {
 fn part_1() {
     let input = read_file("./input.txt".to_string()).unwrap();
 
-    let mut planes = HashMap::<i32, HashMap<i32, HashMap<i32, char>>>::new();
-    planes.insert(0, HashMap::new());
-    let mut lines = input.lines();
-    let mut line = lines.next();
-    let mut y = 0;
-    let z_plane = planes.get_mut(&0).unwrap();
-    while line.is_some() {
-        let y_row = z_plane.entry(y).or_insert_with(|| HashMap::new());
-        let l = line.unwrap();
-        let mut chars = l.chars();
-        let mut c = chars.next();
-        let mut x = 0;
-        while c.is_some() {
-            y_row.insert(x, c.unwrap());
-            x += 1;
-            c = chars.next();
-        }
-        y += 1;
-        line = lines.next();
+    let mut planes = Vec::<Vec<Vec<char>>>::new();
+    planes.push(Vec::<Vec<char>>::new());
+    for line in input.lines() {
+        planes[0].push(line.chars().collect());
     }
-    println!("{:?}", planes);
+    for z in 0..planes.len() {
+        println!("Plane z={}", z);
+        for y in 0..planes[0].len() {
+            for x in 0..planes[0][0].len() {
+                print!("{}", planes[z][y][x]);
+            }
+            println!("");
+        }
+    }
 
-    let mut next_state = planes.clone();
-    for (z_base, z) in planes.iter() {
-        for (y_base, y) in z.iter() {
-            for (x_base, x) in y.iter() {
-                let mut num_active_neighbours = 0;
-                for z_pos in &[-1, 0, 1] {
-                    for y_pos in &[-1, 0, 1] {
-                        for x_pos in &[-1, 0, 1] {
-                            if *z_pos == 0 && *y_pos == 0 && *x_pos == 0 {
-                                continue;
-                            }
-                            if planes.get(&(z_base + z_pos)).is_some()
-                                && planes
-                                    .get(&(z_base + z_pos))
-                                    .unwrap()
-                                    .get(&(y_base + y_pos))
-                                    .is_some()
-                                && planes
-                                    .get(&(z_base + z_pos))
-                                    .unwrap()
-                                    .get(&(y_base + y_pos))
-                                    .unwrap()
-                                    .get(&(x_base + x_pos))
-                                    .is_some()
-                                && *planes
-                                    .get(&(z_base + z_pos))
-                                    .unwrap()
-                                    .get(&(y_base + y_pos))
-                                    .unwrap()
-                                    .get(&(x_base + x_pos))
-                                    .unwrap()
+    let mut next_state;
+    for _ in 0..6 {
+        next_state = planes.clone();
+        for z in 0..planes.len() {
+            for y in 0..planes[0].len() {
+                next_state[z][y].insert(0, '.');
+                next_state[z][y].push('.');
+            }
+            next_state[z].insert(0, vec!['.'; planes[0][0].len() + 2]);
+            next_state[z].push(vec!['.'; planes[0][0].len() + 2]);
+        }
+        next_state.insert(
+            0,
+            vec![vec!['.'; planes[0].len() + 2]; planes[0][0].len() + 2],
+        );
+        next_state.push(vec![vec!['.'; planes[0].len() + 2]; planes[0][0].len() + 2]);
+        planes = next_state.clone();
+
+        // println!("Padded edge");
+        // for z in 0..planes.len() {
+        //     println!("Plane z={}", z);
+        //     for y in 0..planes[0].len() {
+        //         for x in 0..planes[0][0].len() {
+        //             print!("{}", planes[z][y][x]);
+        //         }
+        //         println!("");
+        //     }
+        // }
+
+        for z in 0..planes.len() {
+            for y in 0..planes[0].len() {
+                for x in 0..planes[0][0].len() {
+                    let mut num_active_neighbours = 0;
+                    for z_pos in &[-1i32, 0, 1] {
+                        for y_pos in &[-1i32, 0, 1] {
+                            for x_pos in &[-1i32, 0, 1] {
+                                if *z_pos == 0 && *y_pos == 0 && *x_pos == 0
+                                    || (z as i32 + z_pos) as usize == planes.len()
+                                    || (z as i32 + z_pos) == -1
+                                    || (y as i32 + y_pos) as usize == planes[0].len()
+                                    || (y as i32 + y_pos) == -1
+                                    || (x as i32 + x_pos) as usize == planes[0][0].len()
+                                    || (x as i32 + x_pos) == -1
+                                {
+                                    continue;
+                                }
+                                if planes[(z as i32 + z_pos) as usize][(y as i32 + y_pos) as usize]
+                                    [(x as i32 + x_pos) as usize]
                                     == '#'
-                            {
-                                num_active_neighbours += 1;
+                                {
+                                    num_active_neighbours += 1;
+                                }
                             }
                         }
+                    }
+                    match planes[z][y][x] {
+                        '#' => {
+                            if !(num_active_neighbours == 2 || num_active_neighbours == 3) {
+                                next_state[z][y][x] = '.'
+                            }
+                        }
+                        '.' => {
+                            if num_active_neighbours == 3 {
+                                next_state[z][y][x] = '#'
+                            }
+                        }
+                        _ => panic!("Unknown cube state"),
                     }
                 }
-                // println!("{}", num_active_neighbours);
-                match x {
-                    '#' => {
-                        if !(num_active_neighbours == 2 || num_active_neighbours == 3) {
-                            *next_state
-                                .get_mut(z_base)
-                                .unwrap()
-                                .get_mut(y_base)
-                                .unwrap()
-                                .get_mut(x_base)
-                                .unwrap() = '.';
-                        }
-                    }
-                    '.' => {
-                        if num_active_neighbours == 3 {
-                            *next_state
-                                .get_mut(z_base)
-                                .unwrap()
-                                .get_mut(y_base)
-                                .unwrap()
-                                .get_mut(x_base)
-                                .unwrap() = '#';
-                        }
-                    }
-                    _ => panic!("Unknown cube state"),
+            }
+        }
+
+        planes = next_state.clone();
+        for z in 0..planes.len() {
+            println!("Plane z={}", z);
+            for y in 0..planes[0].len() {
+                for x in 0..planes[0][0].len() {
+                    print!("{}", planes[z][y][x]);
+                }
+                println!("");
+            }
+        }
+    }
+
+    let mut num_active = 0;
+    for z in 0..planes.len() {
+        for y in 0..planes[0].len() {
+            for x in 0..planes[0][0].len() {
+                if planes[z][y][x] == '#' {
+                    num_active += 1;
                 }
             }
         }
     }
 
-    println!("{:?}", next_state);
+    println!("{}", num_active);
 }
